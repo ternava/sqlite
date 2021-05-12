cf7caa3d6061c50812b332a01ced921c03c2f598
ternava <t.xheva@gmail.com>
2021-05-12 09:42:12 +0200

    scripts for measuring binary size and gadgets

---
+++ measures/exe_size.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys, os
+
+from options import all_options
+
+stats_file = "measures/exesize_2-200.csv"
+exe_path = "./sqlite3"
+
+def calculate_stats(exe_path):
+    exe_stats = os.stat(exe_path)
+    print(exe_stats)
+    exe_size = exe_stats.st_size
+    return exe_size
+
+def print_stats(opt): 
+    print(f'Exe size in Bytes is {calculate_stats(exe_path)}')
+    print(f'Exe size in MegaBytes is {calculate_stats(exe_path) / (1024 * 1024)}')
+    print("Exe size when " + 
+        str(opt) + " is: " + 
+        str(calculate_stats(exe_path)) + " bytes, ", 
+        file=open(stats_file, "a"))
+
+def compilesqlite(compile_time_opt):
+    subprocess.run(["make", "clean"])
+    subprocess.run(["./configure"] + compile_time_opt)
+    subprocess.run(["make"])
+
+def do_operations():
+    for opt in all_options:
+        print(opt)
+        compilesqlite(opt)
+        print_stats(opt)
+
+if os.path.exists(stats_file):
+    os.remove(stats_file)
+    do_operations()
+else: 
+    do_operations()
